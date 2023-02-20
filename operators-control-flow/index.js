c2c5fab87c7de90f39f1c1942754d3c42b3eec64
kiprutoilyne <kiprutoilyne@gmail.com>
2023-02-20 18:34:00 +0300

    first commit

---
+++ operators-control-flow/index.js
@@ -0,0 +1,27 @@
+// Question3
+
+let scienceSubject = ('physics,chemistry,Biology,Technical,drawing');
+let generalSubject = ('Eng,math');
+let socialScienceSubject = ('Accounting,commerce,marketing,geography');
+let artSubject = ('economics,Government,literature,history');
+function classGroup(a) {
+
+    if (a = scienceSubject)
+        console.log(scienceSubject);
+
+    else if (a = socialScienceSubject)
+        console.log(socialScienceSubject);
+
+    else if (a = artSubject)
+        console.log(artSubject);
+
+    else (generalSubject)
+    console.log(generalSubject);
+}
+
+
+console.log(scienceSubject)
+console.log(scienceSubject)
+console.log(artSubject)
+console.log(generalSubject)
+
---
+++ operators-control-flow/readme.md
@@ -0,0 +1,47 @@
+# Question 1
+
+## Aritmetic operators
+
+ 1.Addition Operator +
+ 2.Subtraction -
+ 3.Multiplication *
+ 4.Division /
+ 5.Modulus  %
+ 6.Exponention **
+ 7.Increment ++
+ 8.Decrement --
+
+ ## Assignment Operators
+
+ 1.Assignment =
+ 2.Addition assignment +=
+ 3.Multiplication assignment -=
+
+ ## Comparison Operators
+
+1.compares operands ==
+2.not equal to !==
+3.less than <
+4.greater than >
+5.less than or equal to <=
+6.greater than or equal to >=
+
+ ## Logical Operators
+ 1.AND &&
+ 2.OR ||
+ 3.NOT !
+
+ # Questio 4
+
+ for (let i = 1;
+    i < 20;
+    i += 7) {
+    console.log(i);
+}
+
+## answer
+ 1,8,14
+
+
+
+ ## Bitwise Operators
